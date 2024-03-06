766dcab12519293db569a228587e6503ef939493
Shuyang Yao <qzyaoshuyang3@pku.edu.cn>
2024-03-07 04:05:47 +0800

    update

--- Field Analysis/Fig0318 - Independence test for field evolution events.py
+++ Field Analysis/Fig0318 - Independence test for field evolution events.py
@@ -1,2 +1,21 @@
 from mylib.statistic_test import *
-from mylib.stats.indeptest import indept_field_evolution_chi2, indept_field_evolution_mutual_info
+from mylib.field.field_tracker import indept_test_for_evolution_events
+
+code_id = "0318 - Independence test for field evolution events"
+loc = join(figpath, code_id)
+mkdir(loc)
+
+
+
+if os.path.exists(join(figdata, code_id+'.pkl')):
+    with open(join(figdata, code_id+'.pkl'), 'rb') as handle:
+        Data = pickle.load(handle)
+else:
+    Data = DataFrameEstablish(
+        variable_names = ['Training Session', 'Chi-Square Statistic', 'MI', 'Dimension', 'Pair Type', 'Pair Num',
+                          'Paradigm'],
+        f = f_CellReg_modi, 
+        function = IndependentEvolution_Interface, 
+        file_name = code_id, 
+        behavior_paradigm = 'CrossMaze'
+    )
