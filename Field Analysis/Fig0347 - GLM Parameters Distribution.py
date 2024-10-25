14d2d750b3d6aa0ec5c6fafa273994453974d157
Shuyang Yao <qzyaoshuyang3@pku.edu.cn>
2024-10-25 08:37:37 +0800

    update

--- Field Analysis/Fig0347 - GLM Parameters Distribution.py
+++ Field Analysis/Fig0347 - GLM Parameters Distribution.py
@@ -1,2 +1,16 @@
 from mylib.statistic_test import *
 
+code_id = '0347 - GLM Parameters Distribution'
+loc = os.path.join(figpath, code_id)
+mkdir(loc)
+
+
+if os.path.exists(join(figdata, code_id+'.pkl')):
+    with open(join(figdata, code_id+'.pkl'), 'rb') as handle:
+        Data = pickle.load(handle)
+else:
+    Data = {
+        "Behavior Progress": [],
+        "Session ID": [],
+        ""
+    }
