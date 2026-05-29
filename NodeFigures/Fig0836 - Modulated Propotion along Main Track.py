3af5da39cdf5599082fa0079a19d3f9d10dc9c90
Shuyang Yao <qzyaoshuyang3@pku.edu.cn>
2026-05-28 22:38:53 -0400

    update

--- NodeFigures/Fig0836 - Modulated Propotion along Main Track.py
+++
@@ -1,9 +0,0 @@
-from mylib.statistic_test import *
-
-code_id = "0836 - Modulated Proportion along Main Track"
-loc = join(figpath, "Dsp", code_id)
-mkdir(loc)
-
-if exists(join(figdata, code_id+'.pkl')) == False:
-    DataFrameEstablish(variable_names=["Position", "Type", "Proportion"], f=f2, file_name=code_id, 
-                       behavior_paradigm="DSPMaze", function=ModulatedProportionSpatialDistribution_DSP_Interface)
